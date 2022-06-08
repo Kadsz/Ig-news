50609862703d47faff501e65231ec21fd11fbe97
Carlos Eduardo <kadufenix10@gmail.com>
2022-06-08 20:59:19 -0300

    Adicionando Github Provider

--- src/components/ButtonSignGithub/index.tsx
+++ src/components/ButtonSignGithub/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function ButtonSignGithub() {
-  const isUserLogged = true;
+  const { data: session } = useSession();
 
-  return isUserLogged ? (
-    <button className={styles.signButton} type="button">
+  return session ? (
+    <button
+      onClick={() => signOut()}
+      className={styles.signButton}
+      type="button"
+    >
       <FaGithub color="#04d361" />
-      Carlos Eduardo Sousa
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signButton} type="button">
+    <button
+      onClick={() => signIn("github")}
+      className={styles.signButton}
+      type="button"
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
